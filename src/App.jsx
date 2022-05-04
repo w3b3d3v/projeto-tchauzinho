3581015fd80aa72dbae1e4b00dd0bba5a80b354b
Daniel Cukier <>
2022-05-04 13:08:19 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { ethers } from "ethers";
+import './App.css';
+
+export default function App() {
+
+  const wave = () => {
+    
+  }
+  
+  return (
+    <div className="mainContainer">
+
+      <div className="dataContainer">
+        <div className="header">
+        👋 Olá Pessoal!
+        </div>
+
+        <div className="bio">
+        Eu sou o danicuki e já trabalhei com música, sabia? Legal, né? Conecte sua carteira  Ethereum wallet e me manda um tchauzinho!
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Mandar Tchauzinho 🌟
+        </button>
+      </div>
+    </div>
+  );
+}
