4563391580ad145cf0a0a0816291980c42fa9149
Lucas Oliveira <olivmath@protonmail.com>
2022-05-15 02:41:14 -0300

    upadte: get contract events

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,54 @@
-import React from "react";
-import { ethers } from "ethers";
+import React, { useState, useEffect } from "react";
+import { ContractFactory, ethers } from "ethers";
 import './App.css';
+import abi from "./utils/CallCounter.json"
+
+
+const contractAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
+const provider = new ethers.providers.Web3Provider(window.ethereum)
+const contractABI = abi.abi
+const callCounterContract = new ethers.Contract(contractAddress, contractABI, provider.getSigner());
+
+
+export const totalCalls = async () => {
+  let calls = await callCounterContract.getCalls()
+  let callers = await callCounterContract.getCallers()
+
+  console.log(`Called me ${calls.toNumber()} times`)
+  console.log("List of callers")
+  callers.map(caller => console.log(caller))
+}
+
 
 export default function App() {
+  const [userAddress, setUserAddress] = useState("")
+  const [allCalls, setAllCalls] = useState([]);
+  useEffect(() => {
+    const newCall = (caller, indexCall) => {
+      console.log(`${indexCall} - ${caller} call me !`)
+    }
 
-  const call = () => {
-    
+
+    callCounterContract.on("newCall", newCall);
+
+    return () => {
+      if (callCounterContract) {
+        callCounterContract.off("newCall", newCall);
+      }
+    };
+  }, []);
+
+  const connectWallet = async () => {
+    const listUserAddress =  await window.ethereum.request({method: "eth_requestAccounts"})
+    console.info(`✅ Wallet ${listUserAddress[0]} connected!`)
+    setUserAddress(listUserAddress[0])
   }
-  
+
+  const call = async () => {
+    await callCounterContract.call({ gasLimit: 300000 })
+  }
+
+
   return (
     <div className="mainContainer">
 
@@ -24,6 +65,10 @@ export default function App() {
         Me mande um 👋 pela Blockchain.
         </strong>
 
+
+        <button className="callButton" onClick={connectWallet}>
+          <strong>Connectar sua carteira</strong>
+        </button>
         <button className="callButton" onClick={call}>
           <h2>👋 Olá!</h2>
         </button>
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+import App, { totalCalls } from './App'
 
+totalCalls()
 ReactDOM.render(
   <React.StrictMode>
     <App />
