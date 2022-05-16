b4e335650cb9b2f4efb06538396bd20cf38c401e
Lucas Oliveira <olivmath@protonmail.com>
2022-05-16 00:06:36 -0300

    update: decoupling function

--- src/App.jsx
+++ src/App.jsx
@@ -7,36 +7,11 @@ import './App.css';
 const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 const provider = new ethers.providers.Web3Provider(window.ethereum)
 const contractABI = abi.abi
-const callCounterContract = new ethers.Contract(contractAddress, contractABI, provider.getSigner());
-
-
-export const totalCalls = async () => {
-  let calls = await callCounterContract.getCalls()
-  let callers = await callCounterContract.getCallers()
-
-  console.log(`Called me ${calls.toNumber()} times`)
-  console.log("List of callers")
-  callers.map(caller => console.log(caller))
-}
+export const callCounterContract = new ethers.Contract(contractAddress, contractABI, provider.getSigner());
 
 
 export default function App() {
   const [userAddress, setUserAddress] = useState("")
-  const [allCalls, setAllCalls] = useState([]);
-  useEffect(() => {
-    const newCall = (caller, indexCall) => {
-      console.log(`${indexCall} - ${caller} call me !`)
-    }
-
-
-    callCounterContract.on("newCall", newCall);
-
-    return () => {
-      if (callCounterContract) {
-        callCounterContract.off("newCall", newCall);
-      }
-    };
-  }, []);
 
   const connectWallet = async () => {
     const listUserAddress =  await window.ethereum.request({method: "eth_requestAccounts"})
@@ -48,19 +23,6 @@ export default function App() {
     await callCounterContract.call({ gasLimit: 300000 })
   }
 
-  const counterCallers = () => {
-    return [...new Set(allCalls.map(item => item.addr))].map((addr, index) => {
-      const explorer = `https://rinkeby.etherscan.io/address/${addr}`
-      const counter = allCalls.filter(item => addr == item.addr).length
-
-      return (
-        <div key={index}>
-          <br/>
-          <strong><a className="link" target="_blank" href={explorer}>{addr}</a></strong> call me <strong>{counter}</strong> times!
-        </div>
-        )
-    })
-  }
 
   return (
     <div className="mainContainer">
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App, { totalCalls } from './App'
+import App from './App'
+import { totalCalls } from "./Calls"
 
 totalCalls()
 ReactDOM.render(
